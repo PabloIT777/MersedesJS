5613ecfd28b0d10e7fa4e5578bbbf29fb66e2edc
PabloIT777 <87230417+PabloIT777@users.noreply.github.com>
2021-09-22 22:18:18 +0400

    mers
    mers

---
+++ js/accordion.js
@@ -0,0 +1,22 @@
+const lists =document.querySelectorAll('.feature-sub')
+const btns = document.querySelectorAll('.feature__link')
+
+ //   btns.addEventListener('click', () => alert('1'))
+
+ btns.forEach((btnItem, index) => {
+    btnItem .addEventListener('click', () => {
+        btns.forEach((btnItem) => {
+            btnItem.classList.remove('feature__link_active')
+        })
+        
+        btnItem.classList.add('feature__link_active')
+
+        lists.forEach((listItem) => {
+            listItem.classList.add('hidden')
+        })
+
+        lists[index].classList.remove('hidden')
+
+        console.dir(btnItem);
+    })
+ })
